558eb419189d2d8ad55756f34a14d3638ad7ba4a
kwamena-koomson <kwamenakooms@gmail.com>
2024-05-04 19:43:37 +0100

    installed swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,11 @@ router.get('/', userController.getAll);
 
 router.get('/:id', userController.getSingle);
 
+router.post('/', userController.createUser);
+
+router.put('/', userController.updateUser);
+
+router.delete('/', userController.deleteUser);
+
+
 module.exports = router;
